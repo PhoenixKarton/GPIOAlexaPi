6c96d82cf60921eea8879416f56a970ad960269d
Stefan Gerber <stefan-gerber@mail.de>
2020-04-12 18:14:47 +0100

    Added GpioPlugin script

---
+++ GpioPlugin.py
@@ -0,0 +1,27 @@
+import RPi.GPIO as GPIO
+import fauxmo
+
+class GpioPlugin(fauxmo.plugins.FauxmoPlugin):
+ def __init__(self, name, port):
+  super().__init__(name=name, port=port)
+  self.pin = 7
+  self.state = 0
+  GPIO.setmode(GPIO.BOARD)
+  GPIO.setup(self.pin, GPIO.OUT)
+  
+ def on(self) -> bool:
+  GPIO.output(self.pin, GPIO.HIGH)
+  self.state = True
+  return True
+ 
+ def off(self) -> bool:
+  GPIO.output(self.pin, GPIO.LOW)
+  self.state = False
+  return True
+
+ def get_state(self) -> str:
+  if self.state:
+   return "on"
+  elif not self.state:
+   return "off"
+  return "unknown"
